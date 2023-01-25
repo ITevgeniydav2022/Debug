6d1fc1df6fdddcc02eef60875eda27a64c6de4c5
ITevgeniydav2022 <defox2022@gmail.com>
2023-01-25 13:44:26 +0300

    Первый коммит

---
+++ src/main/java/me/davydovep/recipesapp_7/RecipesApp7Application.java
@@ -0,0 +1,13 @@
+package me.davydovep.recipesapp_7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipesApp7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipesApp7Application.class, args);
+    }
+
+}
---
+++ src/test/java/me/davydovep/recipesapp_7/RecipesApp7ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.davydovep.recipesapp_7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesApp7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
